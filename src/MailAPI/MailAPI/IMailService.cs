fa814798522f697591b380b0951496329d2b634d
Gregory Maynard <gregory.maynard@valtech.fr>
2012-07-19 15:31:52 +0200

    Init project

---
+++ src/MailAPI/MailAPI/IMailService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MailAPI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMailService" in both code and config file together.
+    [ServiceContract]
+    public interface IMailService
+    {
+        [OperationContract]
+        bool AddNewUser(string mailingList, string email);
+
+        [OperationContract]
+        bool SendMail(string mailingList, string message);
+    }
+}
---
+++ src/MailAPI/MailAPI/MailService.svc.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using RestSharp;
+
+namespace MailAPI
+{
+    public class MailService : IMailService
+    {
+        public bool AddNewUser(string mailingList, string email)
+        {
+            RestClient client = new RestClient();
+            client.BaseUrl = "https://api.mailgun.net/v2";
+            client.Authenticator =
+                    new HttpBasicAuthenticator("api",
+                                               ConfigurationManager.AppSettings["MAILGUN_API_KEY"]);
+            RestRequest request = new RestRequest();
+            request.Resource = "lists/{list}/members";
+            request.AddParameter("list", mailingList, ParameterType.UrlSegment);
+            request.AddParameter("address", email);
+            request.AddParameter("subscribed", true);
+            request.Method = Method.POST;
+            IRestResponse response = client.Execute(request);
+            return response.ResponseStatus == ResponseStatus.Completed;
+        }
+
+        public bool SendMail(string mailingList, string message)
+        {
+            RestClient client = new RestClient();
+            client.BaseUrl = "https://api.mailgun.net/v2";
+            client.Authenticator =
+                new HttpBasicAuthenticator("api",
+                                           ConfigurationManager.AppSettings["MAILGUN_API_KEY"]);
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain",
+                                 ConfigurationManager.AppSettings["MAIL_DOMAIN"], ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "movies.remember@movies.fr");
+            request.AddParameter("to", mailingList);
+            request.AddParameter("subject", "Sortie ciné");
+            request.AddParameter("html", message);
+            request.Method = Method.POST;
+            IRestResponse response = client.Execute(request);
+            return response.ResponseStatus == ResponseStatus.Completed;
+        }
+    }
+}
+    }
+}
